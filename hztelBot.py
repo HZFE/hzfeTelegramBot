973568c152afd56e733cb2e47c01d5058fd9c624
Geeku <ggongpei@gmail.com>
2017-11-21 23:23:57 +0800

    modules

--- hztelBot.py
+++ hztelBot.py
@@ -1,10 +1,4 @@
-from telegram.ext import 
-    Updater, 
-    CommandHandler, 
-    MessageHandler, 
-    Filters, 
-    InputTextMessageContent,
-    InlineQueryResultArticle
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InputTextMessageContent, InlineQueryResultArticle
 import logging, json
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
