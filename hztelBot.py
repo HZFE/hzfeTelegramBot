bfdf89e980dee7161e8cc1cb6a37652376e0cc6a
Geeku <ggongpei@gmail.com>
2017-11-21 23:19:39 +0800

    inline

--- hztelBot.py
+++ hztelBot.py
@@ -1,4 +1,10 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import 
+    Updater, 
+    CommandHandler, 
+    MessageHandler, 
+    Filters, 
+    InputTextMessageContent,
+    InlineQueryResultArticle
 import logging, json
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -27,10 +33,24 @@ def caps(bot, update, args):
    text_caps = ' '.join(args).upper()
    bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
+def inline_caps(bot, update):
+    query = update.inline_query.query
+    if not query:
+        return
+    result = list()
+    result.append(InlineQueryResultArticle(
+        id=query.upper, 
+        title='Caps', 
+        input_message_content=InputTextMessageContent(query.upper())
+    ))
+    bot.answer_inline_query(update.inline_query.id, results)
+
+
 config = json.load(open('config.json'))
 telBot = TelegramBot(config['TOKEN'])
 telBot.addHandler(CommandHandler, 'start', start)
 telBot.addHandler(CommandHandler, 'caps', caps, pass_args=True)
+telBot.addHandler(InlineQueryHandler, 'caps', inline_caps, pass_args=True)
 telBot.run()
 
 # dispatcher.add_handler(CommandHandler('start', start))
