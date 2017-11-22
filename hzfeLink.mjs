10deb47807d29b34c8f1b341ab8c12a47f4e85b4
Geeku <ggongpei@gmail.com>
2017-11-22 16:33:13 +0800

    links

---
+++ hzfeLink.mjs
@@ -0,0 +1,70 @@
+export default {
+    '清真': {
+        website: 'http://zhaoyuxiang.cn/',
+        github: 'llawlight'
+    },
+    '阿猫': {
+        website: '',
+        github: 'volvo18a'
+    },
+    '小果': {
+        website: '',
+        github: 'uztg'
+    },
+    '陶陶': {
+        website: '',
+        github: 'Taomiaoer'
+    },
+    '夜喵': {
+        website: 'https://nightcat.win/',
+        github: 'NightCatSama'
+    },
+    '地瓜': {
+        website: '',
+        github: 'hellodigua'
+    },
+    '兔哥': {
+        website: 'http://noder.club/',
+        github: 'merrynode'
+    },
+    '树': {
+        website: '',
+        github: 'gongpeione'
+    },
+    '丹丹': {
+        website: 'http://www.funnycoder.cn/',
+        github: 'icemirror'
+    },
+    '阿盖': {
+        website: 'https://blog.xyxiao.cn/',
+        github: 'xyxiao001'
+    },
+    '阿铲': {
+        website: '',
+        github: 'Akiq2016'
+    },
+    '爆栈': {
+        website: 'https://is.daryl.moe/',
+        github: 'Daryl-L'
+    },
+    '小鱼': {
+        website: 'https://blog.wanan.me/',
+        github: 'Yiiu'
+    },
+    '南昌': {
+        website: 'Reusjs',
+        github: ''
+    },
+    '小k': {
+        website: '',
+        github: 'xiaokk06'
+    },
+    '阿铛': {
+        website: '',
+        github: ''
+    },
+    '银马座': {
+        website: '',
+        github: 'yinmazuo'
+    }
+};
--- hztelBot.mjs
+++ hztelBot.mjs
@@ -1,5 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 import config from './config';
+import members from './hzfeLink';
 
 const token = config.TOKEN;
 
@@ -10,7 +11,38 @@ bot.onText(/\/start/, (msg, match) => {
     bot.sendMessage(chatId, 'Hello there!');
 });
 
-bot.on('message', (msg) => {
+function memberLinksToMD(name, links) {
+    return (
+        links.website ? 
+            `${name}的网站是：[${links.website}](${links.website})` : 
+            `${name}还没有网站`
+    ) +
+    '，' + 
+    (
+        links.github ? 
+            `${name}的 Github 是：[@${links.github}](https://github.com/${links.github})` : 
+            `${name}还没有 Github`
+    );
+}
+bot.onText(/\/link\s(.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Message received!');
-});
+    const name = match[1];
+    let returnMsg = {};
+    if (name in members) {
+        returnMsg = members[name];
+    }
+
+    bot.sendMessage(
+        chatId, 
+        memberLinksToMD(name, members[name]), 
+        { parse_mode: 'Markdown' }
+    );
+})
+
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+//     if(msg.indexOf('/') === 0) {
+//         return;
+//     }
+//     bot.sendMessage(chatId, 'Message received!');
+// });
